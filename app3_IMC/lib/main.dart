7be100bbac4248fed151e24ff36d041c250b578c
Wercton <werctonmatheus@gmail.com>
2020-10-12 17:13:58 -0300

    started BMI calculator app

---
+++ app3_IMC/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("BMI-Rechner"),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.refresh), onPressed: () {})
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline, size: 128, color: Colors.deepOrange),
+          ],
+        ));
+  }
+}
