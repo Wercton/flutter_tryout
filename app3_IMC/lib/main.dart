240f9d488c5dc48e6ed2322b31c89cf23a53c8d3
Wercton <werctonmatheus@gmail.com>
2020-10-13 15:35:25 -0300

    layout from initial screen ended

--- app3_IMC/lib/main.dart
+++ app3_IMC/lib/main.dart
@@ -16,19 +16,64 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("BMI-Rechner"),
+          leading: IconButton(
+            icon: Icon(Icons.info_outline),
+            onPressed: () {},
+          ),
+          title: Text("KMI-Rechner"),
           centerTitle: true,
-          backgroundColor: Colors.deepOrange,
+          backgroundColor: Colors.deepPurple,
           actions: <Widget>[
             IconButton(icon: Icon(Icons.refresh), onPressed: () {})
           ],
         ),
         backgroundColor: Colors.white,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(Icons.person_outline, size: 128, color: Colors.deepOrange),
-          ],
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_pin_rounded,
+                  size: 128, color: Colors.deepPurple),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Gewicht (kg)",
+                    labelStyle: TextStyle(color: Colors.deepPurpleAccent[200])),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.deepPurpleAccent[200], fontSize: 20),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Höhe (cm)",
+                  labelStyle: TextStyle(color: Colors.deepPurpleAccent[200]),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.deepPurpleAccent[200], fontSize: 20),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+                child: Container(
+                  height: 50,
+                  child: RaisedButton(
+                    child: Text("Berechnung",
+                        style: TextStyle(color: Colors.white, fontSize: 22)),
+                    onPressed: () {},
+                    color: Colors.deepPurpleAccent,
+                  ),
+                ),
+              ),
+              Text("Über",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.deepPurpleAccent,
+                    fontSize: 22,
+                  ))
+            ],
+          ),
         ));
   }
 }
