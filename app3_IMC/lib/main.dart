ae3aa1dc37c2166d662ace9b0c722bed965535e9
Wercton <werctonmatheus@gmail.com>
2020-10-13 17:44:50 -0300

    imc calculator implemented within formulator

--- app3_IMC/lib/main.dart
+++ app3_IMC/lib/main.dart
@@ -12,6 +12,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController gewichtController = TextEditingController();
+  TextEditingController hoheController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = "Gib Ihre Daten ein";
+
+  void _resetFields() {
+    gewichtController.text = "";
+    hoheController.text = "";
+    setState(() {
+      _infoText = "Gib Ihre Daten ein";
+    });
+  }
+
+  void _berechnung() {
+    double gewicht = double.parse(gewichtController.text);
+    double hohe = double.parse(hoheController.text) / 100;
+    double kmi = gewicht / (hohe * hohe);
+
+    setState(() {
+      if (kmi < 10.0) {
+        _infoText = "Unmöglich (KMI: ${kmi.toStringAsPrecision(1)})";
+      } else if (kmi < 18.5) {
+        _infoText = "Untergewicht (KMI: ${kmi.toStringAsPrecision(2)})";
+      } else if (kmi <= 25) {
+        _infoText = "Normalgewicht (KMI: ${kmi.toStringAsPrecision(2)})";
+      } else if (kmi <= 30) {
+        _infoText = "Übergewicht (KMI: ${kmi.toStringAsPrecision(2)})";
+      } else if (kmi <= 35) {
+        _infoText = "Adipositas I (KMI: ${kmi.toStringAsPrecision(2)})";
+      } else if (kmi <= 40) {
+        _infoText = "Adipositas II (KMI: ${kmi.toStringAsPrecision(2)})";
+      } else {
+        _infoText = "Adipositas III (KMI: ${kmi.toStringAsPrecision(2)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,55 +63,75 @@ class _HomeState extends State<Home> {
           centerTitle: true,
           backgroundColor: Colors.deepPurple,
           actions: <Widget>[
-            IconButton(icon: Icon(Icons.refresh), onPressed: () {})
+            IconButton(icon: Icon(Icons.refresh), onPressed: _resetFields)
           ],
         ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
           padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(Icons.person_pin_rounded,
-                  size: 128, color: Colors.deepPurple),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Gewicht (kg)",
-                    labelStyle: TextStyle(color: Colors.deepPurpleAccent[200])),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.deepPurpleAccent[200], fontSize: 20),
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Höhe (cm)",
-                  labelStyle: TextStyle(color: Colors.deepPurpleAccent[200]),
-                ),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.deepPurpleAccent[200], fontSize: 20),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
-                child: Container(
-                  height: 50,
-                  child: RaisedButton(
-                    child: Text("Berechnung",
-                        style: TextStyle(color: Colors.white, fontSize: 22)),
-                    onPressed: () {},
-                    color: Colors.deepPurpleAccent,
-                  ),
-                ),
-              ),
-              Text("Über",
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(Icons.person_pin_rounded,
+                    size: 128, color: Colors.deepPurple),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Gewicht (kg)",
+                      labelStyle:
+                          TextStyle(color: Colors.deepPurpleAccent[200])),
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    color: Colors.deepPurpleAccent,
-                    fontSize: 22,
-                  ))
-            ],
+                      color: Colors.deepPurpleAccent[200], fontSize: 20),
+                  controller: gewichtController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Bitte, schreiben Sie Ihre Gewicht.";
+                    }
+                  },
+                ),
+                TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Höhe (cm)",
+                      labelStyle:
+                          TextStyle(color: Colors.deepPurpleAccent[200]),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.deepPurpleAccent[200], fontSize: 20),
+                    controller: hoheController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Bitte, schreiben Sie Ihre Höhe.";
+                      }
+                    }),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+                  child: Container(
+                    height: 50,
+                    child: RaisedButton(
+                      child: Text("Berechnung",
+                          style: TextStyle(color: Colors.white, fontSize: 22)),
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _berechnung();
+                        }
+                      },
+                      color: Colors.deepPurpleAccent,
+                    ),
+                  ),
+                ),
+                Text(_infoText,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.deepPurpleAccent,
+                      fontSize: 22,
+                    ))
+              ],
+            ),
           ),
         ));
   }
